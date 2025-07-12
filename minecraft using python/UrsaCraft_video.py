5811da45a5a5ba3eba34fb161b81c9f2104d686b
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2025-07-12 11:21:49 +0545

    Update UrsaCraft_video.py

--- minecraft using python/UrsaCraft_video.py
+++ minecraft using python/UrsaCraft_video.py
@@ -1,15 +1,4 @@
-from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
 
-app = Ursina()
-grass_texture = load_texture('assets/grass_block.png')
-stone_texture = load_texture('assets/stone_block.png')
-brick_texture = load_texture('assets/brick_block.png')
-dirt_texture  = load_texture('assets/dirt_block.png')
-sky_texture   = load_texture('assets/skybox.png')
-arm_texture   = load_texture('assets/arm_texture.png')
-punch_sound   = Audio('assets/punch_sound',loop = False, autoplay = False)
-block_pick = 1
 
 window.fps_counter.enabled = False
 window.exit_button.visible = False
